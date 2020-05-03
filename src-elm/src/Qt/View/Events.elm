4c5785f6921bf86e1f51362957ade823948e9ab3
Martin Janiczek <martin@janiczek.cz>
2020-05-04 01:16:51 +0200

    Add TODO

--- src-elm/src/Qt/View/Events.elm
+++ src-elm/src/Qt/View/Events.elm
@@ -5,6 +5,10 @@ module Qt.View.Events exposing (on, onClick)
 
 Note: signals don't bubble. QT isn't HTML :)
 
+TODO we'll need to use decoders after all, eg. to get stuff from TextInput:
+<https://doc.qt.io/qt-5/qml-qtquick-textinput.html#text-prop>
+I guess the value given to decoders will be the whole object?
+
 -}
 
 import Qt.View.Internal exposing (Attribute(..))
